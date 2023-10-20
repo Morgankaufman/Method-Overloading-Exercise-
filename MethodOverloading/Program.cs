ea3083e51349ce5b97f9b370ab76c330481d524c
Morgan Kaufman <Morgankaufman13@yahoo.com>
2023-10-20 14:58:05 -0400

    finished methods overloading exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = Add(4, 3);
+            Console.WriteLine(answer);
+
         }
+        
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool withDollars)
+        {
+            if(withDollars && (a + b) != 1)
+            {
+                return $"{a + b} dollars";
+            }
+            else if (withDollars && (a + b) == 1)
+            {
+                return $"{a + b} dollar";
+            }
+            else
+            {
+                return $"{a + b}";
+            }
+        }
+
     }
 }
